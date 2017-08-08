6d65fa82bea150a946b9ed93f2fffc242ad724b5
peiyush13 <piyush.wani@amuratech.com>
2017-08-08 18:51:24 +0530

    default parameter added and handled in solution 2

--- assignment2.rb
+++ assignment2.rb
@@ -1,5 +1,8 @@
 def  high_and_low(str="")
+	return nil if str=="" || str==" " 	
 	arr=str.split(" ").map!{|i| i.to_i}
 	"#{arr.max} #{arr.min}"
 end
-p high_and_low("1 -100 3000 1000 10")
+p high_and_low("-1 -100 -3000 -12 -1")
+p high_and_low("")
+p high_and_low(" ") 
