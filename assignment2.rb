a1fc402539ae224928c0e3fb90ea83e1793491b7
peiyush13 <piyush.wani@amuratech.com>
2017-08-08 18:45:31 +0530

    logical bug resolved

--- assignment2.rb
+++ assignment2.rb
@@ -1,5 +1,5 @@
-def  high_and_low(str)
-	arr=str.split(" ")
-	"#{arr.max.to_i} #{arr.min.to_i}"
+def  high_and_low(str="")
+	arr=str.split(" ").map!{|i| i.to_i}
+	"#{arr.max} #{arr.min}"
 end
-p high_and_low("1 2 3 011 10")
+p high_and_low("1 -100 3000 1000 10")
