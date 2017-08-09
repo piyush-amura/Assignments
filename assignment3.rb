47292350a3a8f5f6d2b8d10052280d847d3cbae9
peiyush13 <piyush.wani@amuratech.com>
2017-08-09 14:13:41 +0530

    nil as parameter case handled in assignment 3

--- assignment3.rb
+++ assignment3.rb
@@ -1,7 +1,11 @@
+require 'benchmark'
 # function returns array of sequence of numbers which is sum of consecutive number till that size	
-def sum_of_consecutive_no(size)
+def sum_of_consecutive_no(size=0)
+	return if size.nil?
 	size.negative? ?  Array.new(size.abs+1) { |i| -(i*(i+1)/2)} : Array.new(size.abs+1) { |i| i*(i+1)/2 }
 end	
+	
+puts Benchmark.measure{sum_of_consecutive_no(5)} 
 
 # sample inputs
 # p sum_of_consecutive_no(5)
