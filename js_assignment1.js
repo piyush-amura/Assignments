62aab289e889c7e3d9bf5e8553b165f0dadeee88
peiyush13 <piyush.wani@amuratech.com>
2017-09-13 15:20:18 +0530

    issues resoved and validation added

--- js_assignment1.js
+++ js_assignment1.js
@@ -1,10 +1,11 @@
-var single_numbers = [' ','One ','Two ','Three ','Four ', 'Five ','Six ','Seven ','Eight ','Nine ','Ten ', 'Eleven', 'Twelve ', 'Thirteen ', 'Fourteen', 'Fifteen' , 'Sixteen', 'Seventeen', 'Eighteen', 'Nineteen'];
+var SingleNumbers = [' ','One ','Two ','Three ','Four ', 'Five ','Six ','Seven ','Eight ','Nine ','Ten ', 'Eleven', 'Twelve ', 'Thirteen ', 'Fourteen', 'Fifteen' , 'Sixteen', 'Seventeen', 'Eighteen', 'Nineteen'];
 var tens = ['', 'Twenty', 'Thirty', 'Forty', 'Fifty', 'Sixty','Seventy','Eighty','Ninty'];
-var human_dict = { Crore: 10000000, Lac: 100000,Thousand: 1000,Hundred: 100, Ten: 10, One: 1 };
+var HumanDict = { Crore: 10000000, Lac: 100000,Thousand: 1000,Hundred: 100, Ten: 10, One: 1 };
+
 
 function input_validations(str){
-  var digit_regx_eqn= /^\d*$/;
-  if(digit_regx_eqn.test(str)== false){
+  
+  if(/^\d*$/.test(str)== false){
   	console.log('not a valid number')
   	return false;
   }
@@ -13,6 +14,11 @@ function input_validations(str){
   {
   	console.log('overflow');
   	return false;
+  }
+
+  if(parseInt(str) < 0){
+    console.log('negative no. not allowed');
+    return false;
   }
 }
 
@@ -23,52 +29,56 @@ function plurarize(num){
 
 function assign_string(num){ 
  if (num<100 && num>=20) {
-   num = (tens[parseInt(num/10)-1]+" "+single_numbers[(num%10)]);
+   num = (tens[parseInt(num/10)-1]+" "+SingleNumbers[(num%10)]);
  }
  else if (num<20 && num>=0) {
-   num = single_numbers[num]; 
+   num = SingleNumbers[num]; 
  }
  return num;
 }
 
 String.prototype.humanize = function () {
 	
-  if(input_validations(this)== false){
+  if(input_validations(this.replace(/^0+/,'')) == false){
   	return;    
   }
-  var amount_in_words = [];
-  amount = parseInt(this)
-  var flag=false, reminder, round_up;
-  for (var key in human_dict) {
-    reminder = amount/human_dict[key];
+  var AmountInWords = [];
+  var amount = parseInt(this);
+  var flag = false, reminder, round_up;
+  for (let key in HumanDict) 
+  {
+    reminder = amount/HumanDict[key];
     round_up = parseInt(reminder);
-    if (reminder < human_dict[key] &&  reminder > 1  ) 
+    if (reminder < HumanDict[key] &&  reminder > 1  ) 
     {
-      if(human_dict[key]==10){
-        if(round_up == 1){
-          amount_in_words += single_numbers[(amount%human_dict[key])+10]
-          flag=true
+      //allocating HumanDictionary word for tenth places 
+      if(HumanDict[key] == 10){
+        //for number between 10 and 20
+        if(round_up == 1){     
+          AmountInWords += SingleNumbers[(amount%10) + 10];
+          flag = true;
         }else{
-          amount_in_words += tens[round_up-1] 
+          AmountInWords += tens[round_up-1]; 
         }    
       }
       else{
-       amount_in_words += assign_string(round_up)+" ";
-       amount_in_words += key+plurarize(round_up)+", ";
+       AmountInWords += assign_string(round_up) + ' ';
+       AmountInWords += key + plurarize(round_up) + ', ';
       }
     }
-    if(human_dict[key]==1 && flag == false)                 // condition for unit place 
+    if(HumanDict[key] == 1 && flag == false)                 // condition for unit place 
     {
-      amount_in_words += " "+single_numbers[round_up];
+      AmountInWords += " " + SingleNumbers[round_up];
     }
     if(reminder>1) 
     {
-      amount = amount%human_dict[key];
+      amount = amount%HumanDict[key];
     }  
   }
-  amount_in_words=amount_in_words.trim();
-  if (amount_in_words[amount_in_words.length-1]==','){
-    amount_in_words=amount_in_words.substring(0, amount_in_words.length-1)
+  //remove unnecessary spaces from AmountInWords
+  AmountInWords = AmountInWords.trim();
+  if (AmountInWords[AmountInWords.length-1]==','){
+    AmountInWords=AmountInWords.substring(0, AmountInWords.length-1)
   }
-  console.log(amount_in_words);
+  console.log(AmountInWords);
 };
